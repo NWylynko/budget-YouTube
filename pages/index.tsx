b25e7f818c2769aca70fcfc7ae6f4ae659390c81
Nick Wylynko <nick1014375@gmail.com>
2021-06-09 19:49:21 +0800

    css grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,43 @@
 import styled from "styled-components"
 
+import { VideoItem } from "../components/VideoItem"
+
 export default function HomePage() {
   return (
     <GridContainer>
-
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
+      <VideoItem />
     </GridContainer>
   )
 }
 
 const GridContainer = styled.div`
   display: grid;
+  justify-content: space-around;
+  grid-gap: 16px;
+
+  grid-template-columns: 320px;
+
+  @media (min-width: 700px) {
+    grid-template-columns: 320px 320px;
+  }
+
+  @media (min-width: 1050px) {
+    grid-template-columns: 320px 320px 320px;
+  }
+
+  @media (min-width: 1400px) {
+    grid-template-columns: 320px 320px 320px 320px;
+  }
+
+  @media (min-width: 1750px) {
+    grid-template-columns: 320px 320px 320px 320px 320px;
+  }
 `;
