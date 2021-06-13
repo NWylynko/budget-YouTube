6e72206a8e9aba249aafea9b822a0d2e106430d1
Nick Wylynko <nick1014375@gmail.com>
2021-06-13 21:35:23 +0800

    get comments for a video

--- Database/comment/get.ts
+++ Database/comment/get.ts
@@ -1,4 +1,29 @@
 import db from "../db";
 import SQL from 'sql-template-strings';
 
-export const getComment = async () => {}
+interface Comment {
+  commentId: string;
+  userId: string;
+  timestamp: number;
+  message: string;
+  userName: string;
+  profilePicUrl: string;
+}
+
+export const getComment = async ({ videoId }: { videoId: string }): Promise<Comment[]> => db.get(SQL`
+
+  SELECT 
+    "comments"."commentId", 
+    "comments"."userId", 
+    "comments"."timestamp", 
+    "comments"."message",
+    "users"."userName",
+    "users"."profilePicUrl"
+  FROM 
+    "comments", "users"
+  WHERE 
+    "videoId" = ${videoId}
+  AND
+    "comments"."userId" = "users"."userId";
+
+`);
--- Database/comment/getAll.ts
+++
@@ -1,4 +0,0 @@
-import db from "../db";
-import SQL from 'sql-template-strings';
-
-export const getAllComment = async () => {}
