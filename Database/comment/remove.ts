b9efb4a3d73b231482a9a348c75556bdc39462c8
Nick Wylynko <nick1014375@gmail.com>
2021-06-13 22:02:12 +0800

    db delete comment

--- Database/comment/remove.ts
+++ Database/comment/remove.ts
@@ -2,4 +2,15 @@ import db from "../db";
 import SQL from 'sql-template-strings';
 
 // remove a comment from a video
-export const removeComment = async () => {}
+export const removeComment = async ({ commentId }: { commentId: string }) => {
+
+  await db.run(SQL`
+  
+    DELETE FROM "comments"
+    WHERE "commentId" = ${commentId};
+  
+  `);
+
+  return { commentId }
+
+}
