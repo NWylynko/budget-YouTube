532570110697f70772d2690bf98aca30fec9e695
Nick Wylynko <nick1014375@gmail.com>
2021-06-14 18:53:42 +0800

    add comment for calculateViews

--- Database/views/calculateViews.ts
+++ Database/views/calculateViews.ts
@@ -3,6 +3,12 @@ import SQL from "sql-template-strings";
 
 const fiveMinutes = 1000 * 60 * 60 * 5;
 
+// if the viewsLastCalc timestamp is older than 5 minutes than this function 
+// re-calculates the number of views the video has
+// and updates the row in the database of the video
+// this gets called every time someone watches a video
+// but its unnecessarily to re-calculate this every time a person watches
+// so running at max every 5 minutes is a reasonable middle ground
 export const calculateViews = async ({ videoId }: { videoId: string }) => {
   const { viewsLastCalc } = await db.get<{ viewsLastCalc: number }>(SQL`
     SELECT viewsLastCalc
