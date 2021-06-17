d3279c02af2a30a8c1845f190f9b45e7fb5f7012
Nick Wylynko <nick1014375@gmail.com>
2021-06-17 22:15:03 +0800

    add mysql support

--- Database/db.ts
+++ Database/db.ts
@@ -1,11 +1,51 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import dotenv from 'dotenv'
+dotenv.config()
 
-sqlite3.verbose()
+const openSqlite = async () => {
+  const sqlite3 = await import("sqlite3");
+  const { open } = await import("sqlite")
 
-const db = await open({
-  filename: "./database.db",
-  driver: sqlite3.Database,
-});
+  const db = await open({
+    filename: "./database.db",
+    driver: sqlite3.Database,
+  });
+
+  return db
+}
+
+const openMySQL = async () => {
+  const mysql = (await import('serverless-mysql')).default
+
+  const conn = mysql({
+    config: {
+      host: process.env.MYSQL_HOST,
+      database: process.env.MYSQL_DATABASE,
+      user: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      port: parseInt(process.env.MYSQL_PORT),
+    },
+  })
+
+  const db = {
+    run: conn.query,
+    exec: conn.query,
+    get: conn.query,
+    all: conn.query,
+    close: () => {},
+    open: () => {}
+  }
+
+  return db;
+  
+}
+
+const openDb = async () => {
+  if (process.env.USE_MYSQL === "true") {
+    return openMySQL();
+  }
+  return openSqlite();
+}
+
+const db = await openDb();
 
 export default db;
--- Database/tools/dropTables.ts
+++ Database/tools/dropTables.ts
@@ -1,14 +1,35 @@
 import db from "../db";
 import fs from "fs/promises"
 import Path from "path"
+import SQL from 'sql-template-strings';
 
 export const dropTables = async () => {
-  const path = Path.join(__dirname, '../../../../../database.db')
+
+  try {
+
+    const path = Path.join(__dirname, '../../../../../database.db')
   
-  await db.close()
+    await db.close()
+  
+    await fs.unlink(path)
+  
+    await db.open()
 
-  await fs.unlink(path)
+  } catch (error) {
+    
+    await db.exec(SQL` 
+    
+      drop table if exists history;
+      drop table if exists votes;
+      drop table if exists subscribers;
+      drop table if exists comments;
+      drop table if exists videos;
+      drop table if exists users;
+    
+    `);
 
-  await db.open()
+  }
+
+
 
 }
