2f8247dcdf6d5d945c0f55eac0e958554e3e0bef
Nick Wylynko <nick1014375@gmail.com>
2021-06-20 14:58:06 +0800

    update deps

--- Database/tools/dropTables.ts
+++ Database/tools/dropTables.ts
@@ -1,35 +1,17 @@
 import db from "../db";
-import fs from "fs/promises"
-import Path from "path"
-import SQL from 'sql-template-strings';
+import fs from "fs/promises";
+import Path from "path";
+import SQL from "sql-template-strings";
 
 export const dropTables = async () => {
-
-  try {
-
-    const path = Path.join(__dirname, '../../../../../database.db')
+  await db.exec(SQL` 
   
-    await db.close()
+    drop table if exists history;
+    drop table if exists votes;
+    drop table if exists subscribers;
+    drop table if exists comments;
+    drop table if exists videos;
+    drop table if exists users;
   
-    await fs.unlink(path)
-  
-    await db.open()
-
-  } catch (error) {
-    
-    await db.exec(SQL` 
-    
-      drop table if exists history;
-      drop table if exists votes;
-      drop table if exists subscribers;
-      drop table if exists comments;
-      drop table if exists videos;
-      drop table if exists users;
-    
-    `);
-
-  }
-
-
-
-}
+  `);
+};
--- pages/resetDatabase.tsx
+++ pages/resetDatabase.tsx
@@ -1,20 +1,28 @@
-import { reset } from '../Database/tools/reset';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { reset } from "../Database/tools/reset";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
-  const results = await reset()
+  try {
+    await reset();
+  } catch (error) {
+    return {
+      props: { error },
+    };
+  }
 
   return {
-    props: { results }, // will be passed to the page component as props
-  }
-}
+    props: { done: "success" }, // will be passed to the page component as props
+  };
+};
 
-export default function HomePage({ results }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  
-  console.log(results)
-  
+export default function HomePage(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  console.log(props);
+
   return (
-    <><p>done</p></>
-  )
+    <>
+      <pre>{props}</pre>
+    </>
+  );
 }
