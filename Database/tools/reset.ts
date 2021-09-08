a63cf9fb4444c0768d15e4d7ccbca4a93b28b70b
Nick Wylynko <nick1014375@gmail.com>
2021-09-08 11:29:32 +0800

    add logs

--- Database/tools/reset.ts
+++ Database/tools/reset.ts
@@ -3,7 +3,11 @@ import { createTables } from "./createTables"
 import { dropTables } from "./dropTables"
 
 export const reset = async () => {
+  console.log("dropping tables")
   await dropTables()
+  console.log("creating tables")
   await createTables()
+  console.log("adding test data")
   await addTestData()
+  console.log("done")
 }
